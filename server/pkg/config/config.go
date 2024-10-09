b5b5bbb4b08c57183cfa109ca311dfb4ac28c24b
3062 <372561407@qq.com>
2024-10-09 22:03:03 +0800

    add mysql DB

--- server/pkg/config/config.go
+++ server/pkg/config/config.go
@@ -1,5 +1,26 @@
 package config
 
+import "fmt"
+
 func Load(path string) error {
 	return nil
+}
+
+var C Config
+
+type Config struct {
+	Mysql Mysql
+}
+
+type Mysql struct {
+	User     string
+	Password string
+	Host     string
+	Port     int
+	DBName   string
+}
+
+func (a Mysql) DSN() string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		a.User, a.Password, a.Host, a.Port, a.DBName)
 }
